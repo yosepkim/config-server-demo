af8e8cd783c52d2f49f81ce9ffa9163847f3f738
Yosep Kim <yosepk@gmail.com>
2021-11-04 11:13:33 -0400

    Adds project for the first time!

---
+++ App_Start/ApplicationConfig.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using Steeltoe.Extensions.Configuration.ConfigServer;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using System;
+using System.IO;
+
+namespace ConfigServerDemo
+{
+    public class ApplicationConfig
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        public static void RegisterConfig(string environment)
+        {
+
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(GetContentRoot())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .AddEnvironmentVariables()
+
+                .AddConfigServer(environment);
+
+            Configuration = builder.Build();
+
+        }
+        public static string GetContentRoot()
+        {
+            var basePath = (string)AppDomain.CurrentDomain.GetData("APP_CONTEXT_BASE_DIRECTORY") ??
+               AppDomain.CurrentDomain.BaseDirectory;
+            return Path.GetFullPath(basePath);
+        }
+
+        public static CloudFoundryApplicationOptions CloudFoundryApplication
+        {
+            get
+            {
+                var opts = new CloudFoundryApplicationOptions();
+                var appSection = Configuration.GetSection(CloudFoundryApplicationOptions.CONFIGURATION_PREFIX);
+                appSection.Bind(opts);
+                return opts;
+            }
+        }
+
+        public static CloudFoundryServicesOptions CloudFoundryServices
+        {
+            get
+            {
+                var opts = new CloudFoundryServicesOptions();
+                var serviceSection = Configuration.GetSection(CloudFoundryServicesOptions.CONFIGURATION_PREFIX);
+                serviceSection.Bind(opts);
+                return opts;
+            }
+        }
+    }
+}
